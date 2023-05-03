dca103ad4754afcf929a719729700cc573f9255f
lymperop <jimlibo13@gmail.com>
2023-05-03 18:59:41 +0300

    created empty scripts for the WordNetConnector class and its testing

---
+++ Connectors/WordNetConnector.py
@@ -0,0 +1,5 @@
+"""
+Created 3 May 2023
+@author: Dimitris Lymperopoulos
+Description: A script containing a custom connector class for interaction with WordNet
+"""
---
+++ Testing/test_WNConnector.py
@@ -0,0 +1,20 @@
+"""
+Created 3 May 2023
+@author: Dimitris Lymperopoulos
+Description: A script to test the functionality of the WordNetConnector class
+
+Usage:
+1)  Obtain information about what each method of the connector returns, and how the data are
+    being represented by giving a sample term to search.
+    python3 test_WNConnector.py
+        --search <a string to be searched in the WordNet>
+
+2)  Obtain information about the class and its methods using the default search_term, which is
+    the word <dog>.
+    python3 test_WNConnector.py
+
+Example:
+    python3 test_WNConnector.py
+        --search beautiful
+
+"""
