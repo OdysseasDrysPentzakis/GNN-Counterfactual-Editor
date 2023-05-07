671427994e024d5b83fdd289dd6c265cc6e34ce4
lymperop <jimlibo13@gmail.com>
2023-05-07 21:00:38 +0300

    implemented find_replacements() method

--- Connectors/WordNetConnector.py
+++ Connectors/WordNetConnector.py
@@ -12,4 +12,16 @@ class WordNetConnector:
         pass
 
     def find_replacements(self, term, quantity=1, synonym=False, pos=None):
-        pass
+        synsets = wn.synsets(term)
+        replacements = []
+
+        for synset in synsets:
+            for lemma in synset.lemmas():
+                if lemma.antonyms() and not synonym and (lemma.synset().pos() == pos or pos is None):
+                    replacements.append(lemma.antonyms()[0].name())
+
+                if synonym:
+                    for syn in wn.synsets(lemma.name(), pos=pos):
+                        replacements.extend(lem.name() for lem in syn.lemmas() if lem.name() != term)
+
+        return replacements if quantity >= len(replacements) else replacements[:quantity]
