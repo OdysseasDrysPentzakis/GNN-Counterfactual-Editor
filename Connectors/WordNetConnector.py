e7b41bbc4496d0ce93a998dbb8696749bf7a7803
lymperop <jimlibo13@gmail.com>
2023-05-07 15:44:25 +0300

    initial skeleton for wordnet connector class

--- Connectors/WordNetConnector.py
+++ Connectors/WordNetConnector.py
@@ -3,3 +3,13 @@ Created 3 May 2023
 @author: Dimitris Lymperopoulos
 Description: A script containing a custom connector class for interaction with WordNet
 """
+
+from nltk.corpus import wordnet as wn
+
+
+class WordNetConnector:
+    def __init__(self):
+        pass
+
+    def find_replacements(self, term, quantity=1, synonym=False):
+        pass
