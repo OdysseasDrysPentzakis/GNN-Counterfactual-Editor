64db442f0e8f4103b39a176c971b0045e2174d3e
lymperop <jimlibo13@gmail.com>
2023-05-17 18:54:21 +0300

    fixed issue with over-specific spacy pos tags

--- utils/metrics.py
+++ utils/metrics.py
@@ -4,24 +4,34 @@ Created 17 May 2023
 Description: A python file containing different functions to measure the similarity/distance of two words
 """
 
-from nltk.corpus import wordnet as wn
 import spacy
+from nltk.corpus import wordnet as wn
+
+# TODO: perhaps use a different (larger) model for better and more accurate results
 nlp = spacy.load("en_core_web_sm")
+
+# dictionary that generalizes some specific pos-tags from spacy
+pos_map = {
+    'PROPN': 'NOUN',
+    'PRON': 'NOUN',
+    'AUX': 'VERB'
+}
 
 
 def spacy_similarity(w1, w2):
-    # TODO: pos tagging does not work correctly, maybe use a mapping of all tags to specific pos categories (n, v, etc.)
     tokens = nlp(" ".join([w1, w2]))
-    print(tokens[0].pos_)
-    print(tokens[1].pos_)
-    return tokens[0].similarity(tokens[1]) * (tokens[0].pos_ == tokens[1].pos_)
+    # print(pos_map.get(tokens[0].pos_, tokens[0].pos_))
+    # print(pos_map.get(tokens[1].pos_, tokens[1].pos_))
+    # TODO: handle pos-tags not present in the pos_map.keys()
+    # TODO: perhaps do not use pos-tagging in the computation of the word similarity
+    return tokens[0].similarity(tokens[1]) * (
+            pos_map.get(tokens[0].pos_, tokens[0].pos_) == pos_map.get(tokens[1].pos_, tokens[1].pos_))
 
 
 def wordnet_similarity(w1, w2):
-    # TODO: WordNet pos tagging works better that spacy, could be used as the general pos tagger
     w1, w2 = wn.synsets(w1)[0], wn.synsets(w2)[0]
-    print(w1.pos())
-    print(w2.pos())
+    # print(w1.pos())
+    # print(w2.pos())
     return w1.path_similarity(w2) * (w1.pos() == w2.pos())
 
 
@@ -30,8 +40,8 @@ def mixed_similarity(w1, w2):
 
 
 def main():
-    word1 = "clever"
-    word2 = "charismatic"
+    word1 = "dog"
+    word2 = "cat"
 
     print("Spacy Similarity:", spacy_similarity(word1, word2))
     print("WordNet Similarity:", wordnet_similarity(word1, word2))
