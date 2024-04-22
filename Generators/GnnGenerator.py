1d72b141c300e4019b350c6de3624ca40b11c225
lymperop <jimlibo13@gmail.com>
2024-04-22 14:45:55 +0300

    removed dataset size limiter

--- Generators/GnnGenerator.py
+++ Generators/GnnGenerator.py
@@ -86,7 +86,7 @@ class GnnGenerator:
         if col is None:
             print("[ERROR]: col must be specified")
             exit(1)
-        self.sentences = pd.read_csv(src_file)[[col]].head(100)
+        self.sentences = pd.read_csv(src_file)[[col]]
 
         if gnn_model_file is None:
             print("[ERROR]: gnn_model_file must be specified")
